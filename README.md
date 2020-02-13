deac58df372287ac1a9f843dd75fca6e4829f797
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-13 11:37:19 +0100

    pull things from lhcb/fcc starterkit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Key4HEP 
+
+
+```eval_rst
+.. toctree::
+    :maxdepth: 3
+    :includehidden:
+    :caption: Contents:
+
+    talks-and-presentations/README.md
+    CONTRIBUTING.md
+
+.. toctree::
+    :maxdepth: 2
+    :includehidden:
+    :caption: External links:
+
+    FCC software  <https://cern.ch/fccsw>
+    CLIC software <https://twiki.cern.ch/twiki/bin/view/CLIC/CLICSoftwareComputing>   
+    CEPC software <http://cepcsoft.ihep.ac.cn/>
+```
---
+++ conf.py
@@ -0,0 +1,35 @@
+# You should normally never do wildcard imports
+# Here it is useful to allow the configuration to be maintained elsewhere
+from starterkit_ci.sphinx_config import *  # NOQA
+
+project = 'Key4HEP'
+copyright = '2020, Key4HEP'
+author = 'Key4HEP'
+html_logo = 'starterkit.png'
+
+exclude_patterns += [
+    'archive',
+    'README.md',
+]
+
+html_context = {
+    'display_github': True,
+    'github_user': 'key4hep',
+    'github_repo': 'key4hep-doc',
+    'github_version': 'master',
+    'conf_py_path': '/',
+}
+
+html_static_path += [
+    f'_static',
+]
+
+linkcheck_ignore += [
+]
+
+
+def starterkit_ci_setup(app):
+    app.add_stylesheet('starterkit.css')
+
+
+setup.extra_setup_funcs += [starterkit_ci_setup]
