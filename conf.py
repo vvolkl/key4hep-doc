8fb35894189f2518c1219b05b8130d142169bc5d
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-07 12:28:23 +0200

    [style] copybuttons, minor css tweaks
    [style] copybuttons, minor css tweaks

--- conf.py
+++ conf.py
@@ -2,9 +2,9 @@
 # Here it is useful to allow the configuration to be maintained elsewhere
 from starterkit_ci.sphinx_config import *  # NOQA
 
-project = 'Key4HEP'
-copyright = '2020, Key4HEP'
-author = 'Key4HEP'
+project = 'FCC Starterkit Lessons'
+copyright = '2020, FCC Starterkit'
+author = 'FCC Starterkit'
 html_logo = 'starterkit.png'
 
 exclude_patterns += [
@@ -14,22 +14,27 @@ exclude_patterns += [
 
 html_context = {
     'display_github': True,
-    'github_user': 'key4hep',
-    'github_repo': 'key4hep-doc',
+    'github_user': 'HEP-FCC',
+    'github_repo': 'fcc-tutorials',
     'github_version': 'master',
     'conf_py_path': '/',
 }
+
+extensions = [
+    'sphinx_copybutton',
+    'recommonmark',
+]
 
 html_static_path += [
     f'_static',
 ]
 
 linkcheck_ignore += [
+    # FIXME: The URLs have changed
+    r'https://research\.cs\.wisc\.edu/htcondor/.*',
 ]
-
 
 def starterkit_ci_setup(app):
     app.add_stylesheet('starterkit.css')
-
 
 setup.extra_setup_funcs += [starterkit_ci_setup]
